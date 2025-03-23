076c2cffbf8795e0b61d742b1cc7ede61f8d01ba
dearguru07 <pangalaguruyadav101@gmail.com>
2025-03-23 21:32:08 +0530

    g

--- src/App.js
+++ src/App.js
@@ -11,20 +11,26 @@ const Card=(props)=>{
 
 const App=()=>{
   const [result,setResult]=useState([])
+  const [search,setSeach]=useState('')
   const FetchData=async()=>{
     const data=await fetch("https://dummyjson.com/products?limit=500")
     const json=await data.json()
     console.log(json.products)
     setResult(json?.products)
   }
+  const page_size=10;
+  // const pageNobers=pag
   useEffect(()=>{
     FetchData()
   },[])
+  const pageHandle=()=>{
+    setSeach((prev)=>prev+1)
+  }
   return <div className='App'>
     <h3>Pagination</h3>
     <div className='btn-container'>
       {[...Array(10).keys().map((x)=>{
-        return <button>{x}</button>
+        return <button onChange={(n)=>pageHandle(n)} value={search}>{x}</button>
       })]}
     </div>
     <div className='flex'>
